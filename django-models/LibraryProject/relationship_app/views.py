0c1eea8243c4b750b89ad64935382595502ec561
Branice <braniceadongo@gmail.com>
2025-03-09 14:00:27 -0700

    More

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import BookForm
 
 # Create your views here.
 def list_books(request):
